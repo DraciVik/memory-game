9e7936ac7c58c9c65844c7160ba3ef3a27ad5a8f
DraciVik <viktor.stojanov90@gmail.com>
2020-03-04 23:28:59 +0100

    Separated cards into its own file

---
+++ src/components/cards.js
@@ -0,0 +1,62 @@
+import { faGem, faPaperPlane } from "@fortawesome/free-regular-svg-icons";
+import {
+  faAnchor,
+  faBolt,
+  faCube,
+  faLeaf,
+  faBicycle,
+  faBomb
+} from "@fortawesome/free-solid-svg-icons";
+
+const cards = [
+  {
+    icon: faGem
+  },
+  {
+    icon: faPaperPlane
+  },
+  {
+    icon: faAnchor
+  },
+  {
+    icon: faBolt
+  },
+  {
+    icon: faCube
+  },
+  {
+    icon: faLeaf
+  },
+  {
+    icon: faBicycle
+  },
+  {
+    icon: faBomb
+  },
+  {
+    icon: faGem
+  },
+  {
+    icon: faPaperPlane
+  },
+  {
+    icon: faAnchor
+  },
+  {
+    icon: faBolt
+  },
+  {
+    icon: faCube
+  },
+  {
+    icon: faLeaf
+  },
+  {
+    icon: faBicycle
+  },
+  {
+    icon: faBomb
+  }
+];
+
+export { cards };
